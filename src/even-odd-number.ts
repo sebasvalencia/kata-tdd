903a435b09058303b679032255c1059867bfb0f4
Sebastian Valencia Navarro <sebastian.valencian@gmail.com>
2023-05-01 21:30:51 -0500

    feat: kata 01 - even or odd number

--- src/even-odd-number.ts
+++ src/even-odd-number.ts
@@ -1,3 +1,5 @@
+/**
+ * Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+ */
 
-
-export const evenOrOdd = (number: number) => number % 2 === 0 ? 'Even' : 'Odd';
+export const evenOrOdd = (number: number): "Even" | "Odd" => number % 2 === 0 ? 'Even' : 'Odd';
